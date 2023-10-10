f8a28d30b1a24dfc6b17405eaddfed872a246ee6
Daniel Beneš <78357621+Danny-Benny@users.noreply.github.com>
2023-10-10 19:58:39 +0200

    [progress] styles for navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,19 +3,21 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <nav>
-      <Link to="/">Domů</Link>
-      <ul>
-        <li>
+    <nav className="flex justify-between align-center bg-green-800">
+      <Link to="/" className="m-4 text-2xl font-bold text-white block">
+        Domů
+      </Link>
+      <ul className="flex text-white align-center">
+        <li className="p-2">
           <Link to="/roster">Soupiska týmu</Link>
         </li>
-        <li>
+        <li className="p-2">
           <Link to="/league-matches">Ligové zápasy</Link>
         </li>
-        <li>
+        <li className="p-2">
           <Link to="/galery">Fotogalerie</Link>
         </li>
-        <li>
+        <li className="p-2">
           <Link to="/discussions">Diskuze</Link>
         </li>
       </ul>
