b652a6c2bfab34da91ad8dd22f4658378a77d05c
Daniel Beneš <benes.dan.cz@seznam.cz>
2023-11-21 23:30:13 +0100

    [task] creation of footer and footer added to layout

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  const getCurrentYear = () => {
+    return new Date().getFullYear();
+  };
+
+  return (
+    <div className="flex justify-center items-center bg-mygreen h-20">
+      <p className="text-white">© {getCurrentYear()} SK Dolany</p>
+      <p className="text-white text-sm ml-2">Made with ❤️ by Daniel Beneš</p>
+    </div>
+  );
+};
+
+export default Footer;
