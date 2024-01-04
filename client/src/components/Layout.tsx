0c9347c549750e59131b6374fa36bf362b7dca72
Daniel Beneš <benes.dan.cz@seznam.cz>
2024-01-05 00:07:23 +0100

    [task] center footer

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -8,11 +8,11 @@ type Props = {
 
 const Layout: React.FC<Props> = ({ children }) => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="mx-12">{children}</div>
+      <div className="flex-grow mx-12">{children}</div>
       <Footer />
-    </>
+    </div>
   );
 };
 
